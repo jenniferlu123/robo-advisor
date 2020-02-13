f5186cbc863ab9e7780082c675931b7b02e864b5
jenniferlu123 <59629822+jenniferlu123@users.noreply.github.com>
2020-02-13 17:13:48 -0500

    Write expected headers to csv

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -52,13 +52,22 @@ recent_low = min(low_prices)
 
 csv_file_path = os.path.join(os.path.dirname(__file__),"..", "data", "prices.csv")
 
+csv_headers = ["timestamp", "open", "high", "low", "close", "volume"]
+
 with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
     writer = csv.DictWriter(csv_file, fieldnames=["city", "name"])
     writer.writeheader() # uses fieldnames set above
-    writer.writerow({"city": "New York", "name": "Yankees"})
-    writer.writerow({"city": "New York", "name": "Mets"})
-    writer.writerow({"city": "Boston", "name": "Red Sox"})
-    writer.writerow({"city": "New Haven", "name": "Ravens"})
+    
+    # need some sort of loop
+    writer.writerow({
+        "timestamp": "TODO", 
+        "open": "TODO", 
+        "high": "TODO".
+        "low": "TODO",
+        "close": "TODO",
+        "volume": "TODO"
+    })
+    
 
 # INFO OUTPUTS
 
